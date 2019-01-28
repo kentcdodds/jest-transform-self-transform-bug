ceb228e93e68172063059c50f93af5121daf0381
Kent C. Dodds <kent@doddsfamily.us>
2019-01-28 16:50:48 -0700

    try running tests in watch mode

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   globalSetup: require.resolve('./test/global-setup'),
+  transformIgnorePatterns: ['/node_modules/', 'global-setup\\.js$'],
   transform: {
     '^.+\\.+js$': require.resolve('./test/custom-transform'),
   },
